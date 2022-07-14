0661cf38390ae668135ce4b0f1aca36236896710
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-07-14 18:45:13 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort ( matrix ) {
+  if ( matrix == undefined || matrix.length == 0 ) return [];
+  for (let i = 1; i < matrix.length; i += 2 ) {
+    matrix[ i ] = matrix[ i ].reverse();
+  }
+  let neoMatrix = matrix.toString().split( ',' );
+  return neoMatrix;
 }
